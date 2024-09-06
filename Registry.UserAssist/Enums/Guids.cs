66aa3b256fecf12762eb7e3777661d7f1428d950
Макс Титов <hhit934@gmail.com>
2024-09-07 01:57:27 +0300

    Добавьте файлы проекта.

---
+++ Registry.UserAssist/Enums/Guids.cs
@@ -0,0 +1,18 @@
+﻿namespace Registry.UserAssist.Enums
+{
+    public enum Guids : int
+    {
+        [StringValue("{CEBFF5CD-ACE2-4F4F-9178-9926F41749EA}")]
+        W7_1,
+
+        [StringValue("{F4E57C4B-2036-45F0-A9AB-443BCFE33D9F}")]
+        W7_2,
+
+        [StringValue("{75048700-EF1F-11D0-9888-006097DEACF9}")]
+        XP_1,
+
+        [StringValue("{5E6AB780-7743-11CF-A12B-00AA004AE837}")]
+        XP_2,
+
+    }
+}
